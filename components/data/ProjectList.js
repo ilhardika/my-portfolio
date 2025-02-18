5f35368f5874c543abe24202eeff683083264974
ilhardika <ilhardika67@gmail.com>
2025-02-18 17:57:17 +0700

    remove unused project images and update ProjectList with new cover images

--- components/data/ProjectList.js
+++ components/data/ProjectList.js
@@ -1,48 +1,43 @@
-import Image1 from "~/project1.svg";
-import Image2 from "~/project2.png";
-import Image3 from "~/project3.png";
-import Image4 from "~/project4.png";
-import Image5 from "~/project5.png";
+import coverLP from "~/cover-lp-tech.svg";
+import coverBlog from "~/cover-blog.png";
+import coverMovie from "~/cover-movie-list.png";
+import coverTravapp from "~/cover-travapp.png";
 
 export const ProjectList = [
   {
-    id: 1,
-    image: Image1,
-    title: "Tech Landing Page",
-    desc: "A responsive landing page developed with Tailwind CSS during a web development bootcamp, featuring a modern design, modular class structure, and based on a Figma prototype.",
-    tech: ["HTML", "Tailwind"],
-    link: "https://nextcent-kappa.vercel.app/",
-  },
-  {
-    id: 2,
-    image: Image4,
-    title: "Reqres User List",
-    desc: "A secure user management system with responsive design for any device, featuring dynamic data fetching for real-time updates, enhancing security, and improving user experience.",
+    id: 4,
+    image: coverTravapp,
+    title: "Travel App",
+    desc: "A modern travel booking platform built with React and Vite, designed to provide users with a seamless experience for discovering and booking travel activities.",
     tech: ["HTML", "Tailwind", "React"],
-    link: "https://reqres-users.vercel.app/",
+    link: "https://travapp.vercel.app/",
+    github: "https://github.com/ilhardika/travel-web-app",
   },
   {
     id: 3,
-    image: Image2,
-    title: "Blog",
-    desc: "Converted a Figma design into a responsive web application using Tailwind CSS, featuring a pixel-perfect design, mobile hamburger menu, and key learnings in Tailwind CSS and responsive design.",
-    tech: ["HTML", "Tailwind"],
-    link: "https://dibimbing-blog.vercel.app/",
-  },
-  {
-    id: 4,
-    image: Image5,
+    image: coverMovie,
     title: "Movie Lists",
     desc: "A CRUD application built with React that allows users to manage a movie list using the TMDb API, featuring new movie additions, shortened descriptions, React Router navigation, and a User Info menu for logged-in users.",
     tech: ["HTML", "Bootstrap", "React"],
     link: "https://movie-lists-project.vercel.app/",
+    github: "https://github.com/ilhardika/Movie-List",
   },
   {
-    id: 5,
-    image: Image3,
-    title: "Cafe Landing Page",
-    desc: "A CRUD application built with React that allows users to manage a movie list using the TMDb API, featuring new movie additions, shortened descriptions, React Router navigation, and a User Info menu for logged-in users.",
+    id: 1,
+    image: coverLP,
+    title: "Tech Landing Page",
+    desc: "A responsive landing page developed with Tailwind CSS during a web development bootcamp, featuring a modern design, modular class structure, and based on a Figma prototype.",
     tech: ["HTML", "Tailwind"],
-    link: "https://brewspace-cafe.vercel.app/",
+    link: "https://nextcent-kappa.vercel.app/",
+    github: "https://github.com/ilhardika/landing-page-tech",
+  },
+  {
+    id: 2,
+    image: coverBlog,
+    title: "Blog",
+    desc: "Converted a Figma design into a responsive web application using Tailwind CSS, featuring a pixel-perfect design, mobile hamburger menu, and key learnings in Tailwind CSS and responsive design.",
+    tech: ["HTML", "Tailwind"],
+    link: "https://dibimbing-blog.vercel.app/",
+    github: "https://github.com/ilhardika/dibimbing-blog",
   },
 ];
